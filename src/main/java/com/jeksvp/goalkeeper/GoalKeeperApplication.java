9f9c8a0c523185246a399aae1c154ce17e1889d8
JEKsvp <abadeksvp@gmail.com>
2018-10-12 00:40:04 +0400

    create base of application

---
+++ src/main/java/com/jeksvp/goalkeeper/GoalKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.jeksvp.goalkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalKeeperApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jeksvp/goalkeeper/GoalKeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jeksvp.goalkeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoalKeeperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
